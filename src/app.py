5cda611dcb4157dbdee3a23c623cdd73a562280e
mikjf <michael.jean.flury@gmail.com>
2022-11-07 15:07:10 +0100

    Structure updates

--- src/app.py
+++ src/app.py
@@ -1,7 +1,3 @@
-# install???
-!pip install pmdarima
-!pip install prophet
-
 # standard imports
 import pandas as pd
 import numpy as np
@@ -31,6 +27,12 @@ import plotly.graph_objects as go
 # arima
 from pmdarima.arima import auto_arima
 
+# arima py files
+from get_sum import get_sum
+import auto_arima_single
+from auto_arima_single import run_arima_get_rmse
+from run_arima_tune_get_pred import run_arima
+
 # ets
 from statsmodels.tsa.api import ExponentialSmoothing
 from multiprocessing import cpu_count
@@ -40,15 +42,30 @@ from joblib import delayed
 # prophet
 from prophet import Prophet
 
+# file uploader
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    st.write(dataframe)
+
 # data source and output dir
-data_source = '/content/drive/MyDrive/07 - Six - Team only/Processed data/XXX.csv'
-output_dir = '/content/drive/MyDrive/Auto-ARIMA90/'
+source_file_path = ''
+output_file_path = ''
 
 # LAYOUT
-
-# add title and header
-st.title("Title")
-st.header("Header")
 
 # widget sidebar checkbox to show dataframe
 if st.sidebar.checkbox("Show data source"):
@@ -56,6 +73,10 @@ if st.sidebar.checkbox("Show data source"):
     st.subheader("Subheader:")
     st.dataframe(data=data_source.head())
     #st.table(data=data_source)
+
+# add title and header
+st.title("Title")
+st.header("Header")
 
 # setting up columns
 #left_column, middle_column, right_column = st.columns([3, 1, 1])
