2884eecb0c21d7775cc14f78c87522d39514a936
mikjf <michael.jean.flury@gmail.com>
2022-11-07 11:35:17 +0100

    Setup

---
+++ notebooks/app.py
@@ -0,0 +1,14 @@
+# Standard imports
+import pandas as pd
+
+# matplotlib
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+#plotly
+import plotly.express as px
+import plotly.graph_objects as go
+
+import streamlit as st
+
+st.title("HELLO")
